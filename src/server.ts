a335934748cbcad152e9a9d5088b043cb2bc7c2e
Ali Mohamed <amohamed@onesolutionc.com>
2023-01-30 08:50:52 +0200

    adding prisma schema

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-const express=require('express');
+import express from'express';
 const app = express();
 app.get('/',(req,res)=>{
     console.log('hello from express');
     res.status(200);
     res.json({message:'Hello'});
 })
-module.exports  =app; 
+export{app}; 
